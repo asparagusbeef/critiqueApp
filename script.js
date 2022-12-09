16e0e71a7b3809bc06db8aadf9a195641f7a8fdd
jonathanoren <113572672+jonathanoren@users.noreply.github.com>
2022-12-09 05:15:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$('.like, .dislike').on('click', function() {
+    event.preventDefault();
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+});
+
+function onsubmit() {
+  var x = document.getElementById("rating");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
